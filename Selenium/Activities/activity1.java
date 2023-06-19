ad047afa1941bbd66a07001101ea270e84501190
surekhs1 <135097948+surekhs1@users.noreply.github.com>
2023-06-19 20:34:45 +0530

    Add files via upload

---
+++ Selenium/Activities/activity1.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class activity1 {
+    public static void main(String[] args) {
+        // Set up Firefox driver
+        WebDriverManager.firefoxdriver().setup();
+        // Create a new instance of the Firefox driver
+        WebDriver driver = new FirefoxDriver();
+
+        // Open the page
+        driver.get("https://www.training-support.net");
+        // Print the title of the page
+        System.out.println("Home page title: " + driver.getTitle());
+
+        // Find About Us link using id and click it
+        driver.findElement(By.id("about-link")).click();
+        // Print the title of the new page
+        System.out.println("About page title: " + driver.getTitle());
+
+        // Close the browser
+        driver.close();
+    }
+}
